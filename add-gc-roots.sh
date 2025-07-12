3c169d9d44a3e3a4e53acb4c034e1f423f686b52
Jacob Birkett <jacob@birkett.dev>
2025-07-11 21:05:06 -0700

    add-gc-roots: init script to add roots from flake-derivations.nix

---
+++ add-gc-roots.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+flake_path=$(realpath "${1:-$(pwd)}")
+flake_dirname=$(basename "$flake_path")
+
+flake_drvs_expr_file=$(realpath "$(dirname "${BASH_SOURCE[0]}")/flake-derivations.nix")
+flake_drvs_expr="import $flake_drvs_expr_file builtins.currentSystem \"$flake_dirname\""
+
+readarray -t flake_drvs < <(
+	nix eval --option pure-eval false --impure --json \
+		--override-flake "$flake_dirname" "$flake_path" \
+		--expr "$flake_drvs_expr" \
+		| jq -r '.[]'
+)
+
+for store_path in "${flake_drvs[@]}"; do
+	drv_name=$(basename "$store_path")
+	nix-store --add-root "/tmp/$drv_name" --realise "$store_path"
+done
